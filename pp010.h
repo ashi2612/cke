df732cb4698a93b6c1635b0880221cafa347875a
Ashi Aggarwal <78086319+ashi2612@users.noreply.github.com>
2021-01-29 22:11:15 +0530

    Add files via upload

---
+++ pp010.h
@@ -0,0 +1,8 @@
+/*pp010: Create a header file as pp010.h having a function iseven() 
+which takes a number as input and returns 1 if the number is even else returns 0.
+ Write a program pp010.c, include the header file into 
+ */
+ 
+int iseven(int x){	
+	return (x%2?0:1);	
+}
